7fc6f458d0466b7d2912960c9f6e1375054ebc5e
chengkaizone <chengkaizone@163.com>
2017-11-06 17:23:48 +0800

    add code

---
+++ DucomentConverter/CommentInfo.swift
@@ -0,0 +1,64 @@
+//
+//  CommentInfo.swift
+//  DucomentConverter
+//
+//  Created by tony on 2017/11/6.
+//  Copyright © 2017年 chengkaizone. All rights reserved.
+//
+
+import Cocoa
+
+class CommentInfo: NSObject {
+    
+    var title: String!
+    var content: String!
+    var keywords: String = ""
+    // 标记序号
+    var markSerial: Int32 = 0
+    
+    // 根据一行数据初始化
+    init(line: String, keywords: String, markSerial: Int32) {
+        let arr = line.components(separatedBy: "\n")
+        
+        self.title = arr[0].replacingOccurrences(of: "\n", with: "").replacingOccurrences(of: "\r", with: "").replacingOccurrences(of: "\t", with: "")
+        self.content = arr[1].replacingOccurrences(of: "\n", with: "").replacingOccurrences(of: "\r", with: "").replacingOccurrences(of: "\t", with: "")
+        
+        self.keywords = keywords
+        self.markSerial = markSerial
+        
+        super.init()
+    }
+    
+    init(title: String, content: String, keywords: String, markSerial: Int32) {
+        
+        self.title = title
+        self.content = content
+        self.keywords = keywords
+        self.markSerial = markSerial
+        super.init()
+    }
+    
+    func lineString() -> String {
+ 
+        let encodedLength = self.content.endIndex.encodedOffset - self.content.startIndex.encodedOffset
+        // 插入mark和关键词的位置
+        var pos0 = Int(arc4random()) % (encodedLength * 4 / 5)
+        var pos1 = Int(arc4random()) % (encodedLength * 4 / 5)
+        
+        pos0 = pos0 < pos1 ? pos0 : pos1
+        if pos0 == pos1 {
+            pos1 += 2
+        }
+        
+        let posIndex0 = self.content.index(content.startIndex, offsetBy: pos0)
+        let posIndex1 = self.content.index(content.startIndex, offsetBy: pos1)
+        let start = self.content.substring(to: posIndex0)
+        let middle = self.content.substring(with: posIndex0 ..< posIndex1)
+        let end = self.content.substring(from: posIndex1)
+        
+        let tmpContent = start + "\(self.keywords)" + middle + "\(markSerial)" + end
+
+        return tmpContent + "\t" + self.title + "\n"
+    }
+
+}
--- DucomentConverter/ViewController.swift
+++ DucomentConverter/ViewController.swift
@@ -17,41 +17,93 @@ class ViewController: NSViewController {
     
     var data: [ServerSite]!
     
+    var keywords: String = "视频编辑"
+    var markSerial: Int32 = 12
+    var commentData: [CommentInfo]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.data = [ServerSite]()
+        self.readComment()
+    }
+    
+    fileprivate func readComment() {
         
-        let path = Bundle.main.path(forResource: "serversite", ofType: "txt")!
+        self.commentData = [CommentInfo]()
+        let path = Bundle.main.path(forResource: "content", ofType: "txt")!
         
         NSLog("path: \(path)")
-       
+        
         do {
             
             let data = try Data(contentsOf: URL(fileURLWithPath: path))
-            
             let result = String.init(data: data, encoding: String.Encoding.utf8)
-            
             NSLog("result: \(result)")
-            
             readTextView.string = result
             
-            
-            //readTextView.textStorage?.append(result)
         } catch let error {
             
         }
-        
     }
-
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
+    
+    fileprivate func readCommentStr() {
+        
+        guard let str = readTextView.string else {
+            
+            return
+        }
+        
+        self.commentData.removeAll()
+        
+        let lineStrs = str.components(separatedBy: "\n\n")
+        
+        countLabel.stringValue = String(lineStrs.count)
+        
+        for i in 0 ..< lineStrs.count {
+            
+            let lineStr = lineStrs[i]
+            
+            if lineStr == "" {
+                continue
+            }
+            
+            NSLog("line result:  \(i)   >>|\(lineStr)|<")
+            let info = CommentInfo(line: lineStr, keywords: keywords, markSerial: markSerial)
+            
+            self.commentData.append(info)
+        }
+    }
 
     
     @IBAction func readAction(_ sender: Any) {
+        
+        self.readCommentStr()
+    }
+    
+    @IBAction func outAction(_ sender: Any) {
+        
+        if self.commentData.count == 0 {
+            
+            return
+        }
+        
+        var result: String = ""
+        for info in self.commentData {
+            result.append(info.lineString())
+        }
+        outTextView.string = result
+        // 写入文件
+        
+        //NSLog("\(result)")
+    }
+    
+    fileprivate func readServerSite() {
         
         guard let str = readTextView.string else {
             
@@ -80,8 +132,31 @@ class ViewController: NSViewController {
         
     }
     
-    @IBAction func outAction(_ sender: Any) {
+    fileprivate func testServerSite() {
+        self.data = [ServerSite]()
         
+        let path = Bundle.main.path(forResource: "serversite", ofType: "txt")!
+        
+        NSLog("path: \(path)")
+        
+        do {
+            
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            
+            let result = String.init(data: data, encoding: String.Encoding.utf8)
+            
+            NSLog("result: \(result)")
+            
+            readTextView.string = result
+            
+            
+            //readTextView.textStorage?.append(result)
+        } catch let error {
+            
+        }
+    }
+    
+    fileprivate func exportServerSite() {
         if self.data.count == 0 {
             
             return
@@ -95,10 +170,6 @@ class ViewController: NSViewController {
         }
         
         outTextView.string = result
-        
-        // 写入文件
-        
-        
     }
 
 }
